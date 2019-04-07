73c6243d1a5e6975b18c0271efc7c90eb4534937
MuhammetD <doktor.oglu333@gmail.com>
2019-04-06 21:51:37 -0500

    changed a few js and fixed the results background

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,38 @@
-var seconds = 10;   
+var seconds = 31;   
 
-function game(){
+function game() {
     
-	var question1 = document.quiz.question1.value;
-	var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question4.value;
-    var correct = 0;
-    
-
-
-	if (question1 === "t") {
-		correct++;
-}
-	if (question2 === "t") {
-		correct++;
-}	
-	if (question3 === "t") {
-		correct++;
-}
-    if (question4 === "t") {
-		correct++;
+	var questions = [];
+	questions.push(document.quiz.question1.value);
+	questions.push(document.quiz.question2.value);
+	questions.push(document.quiz.question3.value);
+	questions.push(document.quiz.question4.value);
+	
+    var score = 0;
+	
+	for (var x = 0; x < questions.length ; x++) {
+		if (questions[x] === "t") score++;
 	}
 	
-	var pictures = ["./assets/images/wellDone.gif", "./assets/images/meeehh.gif", "./assets/images/betterLckNxtTme.gif", "./assets/images/terrible.gif"];
-	var messages = ["Well Done!", "You are fine:)", "Better luck next time...", "You are terrible!!"];
-	var score;
-
-	if (correct === 4) {
-		score = 0;
-    }
-    if (correct === 3) {
-		score = 1;
-	}
-
-	if (correct === 2) {
-		score = 2;
-	}
-
-	if (correct === 1) {
-		score = 3;
-    }
-    
-
-
+	var pictures = ["./assets/images/terrible.gif","./assets/images/terrible.gif","./assets/images/betterLckNxtTme.gif","./assets/images/meeehh.gif","./assets/images/wellDone.gif",]
+	var messages = ["Nope, do it again!!","You are terrible!!","Better luck next time...","You are fine:)","Well Done!"]
+	
     document.getElementById("results").style.visibility = "visible";
 	document.getElementById("message").innerHTML = messages[score];
-	document.getElementById("correct-answers").innerHTML = "You got " + correct + " correct answers!!";
+	document.getElementById("correct-answers").innerHTML = "You got " + score + " correct answers!!";
 	document.getElementById("image").src = pictures[score];
     }
     
     function timer(){
         
         seconds = seconds - 1;
-        if(seconds < 10) {
+        if(seconds < 31) {
             time.innerHTML = seconds;
-            console.log(seconds);
         }
         if (seconds < 1){
-            window.clearInterval(update);
-            quiz.innerHTML = "Game Over";
-            $('#button').on('click', stop.time);
+			window.clearInterval(update);
+			game();
+            
         }
     }
 
